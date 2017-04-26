6812a942d36159649da87ad69cc3aa676c85342b
Justin <justmas@gmail.com>
2017-04-25 22:05:47 -0500

    initial commit

---
+++ src/main/java/examples/Example3.java
@@ -0,0 +1,22 @@
+package examples;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Example3 {
+	void addFirst(List<?> list) {
+		Object x = list.get(0);
+		list.add(x); // ??????
+		
+		? t = list.get(0); // ? is not a valid identifier
+	}
+	
+	<T> void addFirstCapture(List<T> list) {
+		Object x = list.get(0);
+		list.add(x); // still doesn't work
+		
+		T t = list.get(0); // T is not a valid identifier
+		list.add(t); // but this does, thanks to capture
+	}
+
+}
